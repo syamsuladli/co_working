35a7a644c0fef1666d66b9bc94435994f0363fdf
syamsuladli <45966861+syamsuladli@users.noreply.github.com>
2018-12-30 11:37:52 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     public function registerUser(Request $req){
-
+        //Register new user
         try{
             $user = new User;
             $user->name = $req->name;
@@ -28,7 +28,7 @@ class UserController extends Controller
 
     public function listUser(){
     	
-       
+       //list data for users
         $users = new User;
         $users = DB::table('users')->select('name', 'email as user_email')->get();
 
@@ -36,10 +36,9 @@ class UserController extends Controller
     }
 
     public function checkIn(Request $req){
-    	
-            
+        //log user come in
           
-            $userlog = new UserLog;//::where('user_id', $req);        
+            $userlog = new UserLog;       
             $userlog->user_id = $req->user_id;
             $userlog->checkin_at=date('Y-m-d h:i:s');
             $userlog->checkout_at=date('Y-m-d h:i:s');
@@ -47,16 +46,17 @@ class UserController extends Controller
             echo 'Check In at :';
             
            
-            return date('Y-m-d h:i:s');
+            return date('Y-m-d h:i:s');//displays current date
         }
 
 
     public function checkOut(Request $req){
-
-        $userlog = new UserLog;//::where('user_id', $req);        
+        //log user come out
+        
+            $userlog = new UserLog;       
             $userlog->user_id = $req->user_id;
-            $userlog->checkin_at=date('Y-m-d h:i:s');
             $userlog->checkout_at=date('Y-m-d h:i:s');
+            $userlog->checkin_at=date('Y-m-d h:i:s');
             $userlog->save();
             echo 'Check Out at :';
     	return date('Y-m-d h:i:s');
